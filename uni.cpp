455a6eaa93a34e9243e650dd0dfa8fa0f600ff71
melvinzhang <melvinzhang@gmail.com>
2016-12-29 10:37:24 +0800

    prevent long chains of forwarding closures that take up linear memory when evaluating inf.blc

--- uni.cpp
+++ uni.cpp
@@ -346,9 +346,15 @@ int run() {
             //resolve v to an closure clo and continue execution
             //with t = clo->t and e = clo->e
             C* clo = deref(T[t+1]);
-            while ((collapse || shortcircuit) && clo->t == FORWARD) {
-                clo = clo->e.get();
-                forward++;
+            if (collapse || shortcircuit) {
+                C* start = clo;
+                while (clo->t == FORWARD) {
+                    clo = clo->e.get();
+                    forward++;
+                }
+                if (start->t == FORWARD && start->e.get() != clo) {
+                    start->e = clo;
+                }
             }
             t=clo->t;
             e=clo->e;
